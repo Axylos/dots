7ec7c3dd74f0efe1aeef3d62818f580d356f87b3
cloud-init created default user <ec2-user@ip-172-31-19-167.us-west-2.compute.internal>
2014-11-19 03:42:36 +0000

    update script and add tmux

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-mv ./vimrc ~/.vimrc
-mv ./prof ~/.profile
-mv ./tmux-conf ~/.tmux-conf
+cp ./vimrc ~/.vimrc
+cp ./prof ~/.profile
+cp ./tmux.conf ~/.tmux.conf
+sudo zypper install gcc
+sudo zypper install make
+#curl -fsSL https://gist.github.com/shime/5706655/raw/install.sh | sudo bash -e
+exec /bin/bash --login
